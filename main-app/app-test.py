f96a6b94fc7f9729f2c0df37447b88584598b539
cosmas28 <augubilla100@gmail.com>
2018-03-01 08:19:18 +0300

    Add testcase for register business functionality

--- main-app/app-test.py
+++ main-app/app-test.py
@@ -1,7 +1,7 @@
 import unittest
 
 from app import app
-
+from models import Business
 
 class ConfigTestCase(unittest.TestCase):
 
@@ -10,6 +10,14 @@ class ConfigTestCase(unittest.TestCase):
         run_flask = app.test_client()
         response = run_flask.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
+
+
+class BusinessTestCase(unittest.TestCase):
+    def setUp(self):
+        self.business = Business('1', 'cosmas', 'Cosma Tech', 'Nairobi', 'Technology')
+
+    def test_register_business(self):
+        self.assertTrue(self.business.create_business())
 
 
 if __name__ == '__main__':
