7b577baac76f8ac43ddb87df3548b2f66a398560
cosmas28 <augubilla100@gmail.com>
2018-03-06 21:52:57 +0300

    Design test case to test view all business API endpoint

---
+++ tests/test_api_endpoints.py
@@ -0,0 +1,40 @@
+"""Design test case to test business related endpoints.
+
+This module design test suite into which contains test cases for behaviors that
+are expected from API endpoints.
+
+"""
+
+
+
+import unittest
+
+from app import app
+from src.models.business import Business
+
+
+class TestBusinessEndpointsTestCase(unittest.TestCase):
+    """Illustrate test cases to test expected behavior of business API endpoints. """
+
+    def setUp(self):
+        """Enter business records int business records dictionary so that it can be reused by other test cases."""
+        self.run_app = app.test_client()
+        self.business = Business()
+        self.business.create_business(1, 'cosmas', 'Cosma Tech', 'Nairobi', 'Technology', 'Masters of ecommerce')
+        self.business.create_business(2, 'Allan', 'Allan Tech', 'Kitale', 'Technology', 'Cryptocurrency')
+
+    def tearDown(self):
+        """Delete registered business records after every test case has run."""
+
+        for key in self.business.business_records:
+            del self.business.business_records[key]
+
+    def test_view_businesses_endpoint(self):
+        """Test whether a get request to business API endpoint has succeeded."""
+
+        response = self.run_app.get('/api/v1/business')
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
