7b382fd0daa78022007e7b9a4c89eec886765e01
cosmas28 <augubilla100@gmail.com>
2018-03-08 00:19:00 +0300

    Use persistence quotation marks [#155568718]

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -19,7 +19,7 @@ class UserRegistrationTest(unittest.TestCase):
         """Instantiate the User class so that it can be reused by other test cases."""
 
         self.user = User()
-        self.user.register_user('cosmas28', 'password123')
+        self.user.register_user("cosmas28", "password123")
 
     def tearDown(self):
         self.user.registered_users.remove(0)
@@ -32,27 +32,27 @@ class UserRegistrationTest(unittest.TestCase):
     def test_duplicate_username(self):
         """Test username already exist."""
 
-        test_resonse = self.user.register_user('cosmas28', 'mark123444')
+        test_resonse = self.user.register_user("cosmas28", "mark123444")
         self.assertEqual(test_resonse, "The username already exist")
 
     def test_password_length(self):
         """Test user password to be more than 6 characters."""
 
-        test_response = self.user.register_user('facebook', 'net23')
+        test_response = self.user.register_user("facebook", "net23")
         self.assertEqual(test_response, "Password must be more that 6 characters!")
 
     def test_one_user_input_missing(self):
         """Test user input one field."""
 
-        test_response = self.user.register_user('yoyo2018')
+        test_response = self.user.register_user("yoyo2018")
         self.assertEqual(test_response, "Both username and password is requird!")
 
     def test_success_user_registration(self):
         """Test user registration is successful."""
 
-        test_response = self.user.register_user('augustino28', 'andela2018')
+        test_response = self.user.register_user("augustino28", "andela2018")
         self.assertEqual(test_response, "Successful registered")
-        self.assertIn('augustino28', test_response["user_list"])
+        self.assertIn("augustino28", test_response["user_list"])
 
 
 if __name__ == '__main__':
