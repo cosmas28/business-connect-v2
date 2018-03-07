f1e57e673c10a4b3b1c042d81da9f8212e6a7b8b
cosmas28 <augubilla100@gmail.com>
2018-03-07 21:26:17 +0300

    Add test case for business registration API endpoint

--- tests/test_api_endpoints.py
+++ tests/test_api_endpoints.py
@@ -20,14 +20,28 @@ class TestBusinessEndpointsTestCase(unittest.TestCase):
         """Enter business records int business records dictionary so that it can be reused by other test cases."""
         self.run_app = app.test_client()
         self.business = Business()
+        self.business_record = {
+            "business_id": 1,
+            "business_owner": "Cosmas",
+            "business_name": "Cosma Tech",
+            "business_category": "Technology",
+            "business_location": "Arusha",
+            "business_summary": "Internet of things is making the world a better place"
+        }
         self.business.create_business(1, 'cosmas', 'Cosma Tech', 'Nairobi', 'Technology', 'Masters of ecommerce')
         self.business.create_business(2, 'Allan', 'Allan Tech', 'Kitale', 'Technology', 'Cryptocurrency')
 
     def tearDown(self):
         """Delete registered business records after every test case has run."""
 
-        for key in self.business.business_records:
+        for key in list(self.business.business_records.keys()):
             del self.business.business_records[key]
+
+    def test_register_business_endpoint(self):
+        """Test business API endpoint can register new business with POST request."""
+
+        response = self.run_app.post('/api/v1/business', data=self.business_record)
+        self.assertEqual(response.status_code, 201)
 
     def test_view_businesses_endpoint(self):
         """Test whether a get request to business API endpoint has succeeded."""
@@ -38,7 +52,7 @@ class TestBusinessEndpointsTestCase(unittest.TestCase):
     def test_view_businesses_by_id_endpoint(self):
         """Test whether providing business id to a get request to business API endpoint has succeeded."""
 
-        response = self.run_app.get('/api/v1/business/1')
+        response = self.run_app.get('/api/v1/businesses/1')
         self.assertEqual(response.status_code, 200)
 
 
