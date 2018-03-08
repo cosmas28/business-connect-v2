fb28c19896c950e2843e3f9c951474556504bada
cosmas28 <augubilla100@gmail.com>
2018-03-08 11:07:07 +0300

    Import user API endpoint to the app module [#155568718]

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask
 
 
 from src.resources.business import business_api
-
+from src.resources.user import user_api
 
 app = Flask(__name__)
 app.register_blueprint(business_api, url_prefix='/api/v1')
+app.register_blueprint(user_api, url_prefix='/api/v1')
 
 
 @app.route('/')
