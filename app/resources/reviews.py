97d1da62ca18034e95061b13b9d70e8b1ac74c43
cosmas28 <augubilla100@gmail.com>
2018-03-16 22:40:30 +0300

    Develop add reviews API endpoint [#155573993]

---
+++ app/resources/reviews.py
@@ -0,0 +1,51 @@
+"""Demonstrate all business reviews related API endpoints.
+
+This module provides API endpoints to add business reviews and view reviews fo a single business.
+
+"""
+
+import datetime
+from flask import Blueprint, abort, request
+
+from flask_restful import (Resource, Api, reqparse)
+
+from app.models.reviews import Reviews
+
+
+reviews = Reviews()
+
+
+class BusinessReviews(Resource):
+
+    """Illustrate API endpoints to add and view business reviews."""
+
+    def post(self, business_id):
+        """Add a business review.
+
+        Args:
+            business_id (int): business id parameter should be unique to identify each business.
+
+        Returns:
+            A success message to indicate review was added successfully.
+
+        Raises:
+            KeyError when the business id does not exist.
+            Error message when business review is empty.
+            Error message when no business id was provided.
+
+        """
+        request_data = request.get_json()
+        business_review = request_data['review']
+        created_at = datetime.datetime.now().strftime("%y-%m-%d")
+
+        response = reviews.add_review(business_id, business_review, created_at)
+        return response, 201
+
+
+reviews_api = Blueprint('resources.reviews', __name__)
+api = Api(reviews_api)
+api.add_resource(
+    BusinessReviews,
+    '/businesses/<int:business_id>/reviews',
+    endpoint='reviews'
+)
