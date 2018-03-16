bc0df629d0dd0b9890a87bf521727ade8c9bd67f
cosmas28 <augubilla100@gmail.com>
2018-03-17 02:50:42 +0300

    Design test case for view business reviews method [#155568204]

--- tests/test_review_model.py
+++ tests/test_review_model.py
@@ -49,5 +49,42 @@ class AddReviewTest(unittest.TestCase):
         self.assertEqual(self.reviews.add_review(1, 'first review', '16-03-107'), 'Business review added successfully!')
 
 
+class ViewBusinessReviewsTest(unittest.TestCase):
+
+    """Illustrate test cases to test expected behavior of view business reviews functionality. """
+
+    def setUp(self):
+        """Instantiate the Reviews class so that it can be reused by other test cases."""
+
+        self.reviews = Reviews()
+        business.create_business(1, 'Cosmas', 'Cosma Tech', 'Nairobi', 'Technology', 'Masters of ecommerce')
+
+    def tearDown(self):
+        """Delete registered business records after every test case has run."""
+
+        del business.business_records[1]
+
+    def test_empty_business_id(self):
+        """Test whether business id is empty."""
+
+        self.assertEqual(self.reviews.view_business_reviews(''), 'Business id is required!')
+
+    def test_business_id_existence(self):
+        """Test if a Error message will be raised when the business id does not exist."""
+
+        self.assertEqual(self.reviews.view_business_reviews(2), 'Business ID does not exist!')
+
+    # def test_empty_business_reviews(self):
+    #     """Test whether business have no reviews."""
+    #
+    #     self.assertEqual(self.reviews.view_business_reviews(1), 'Sorry! This business have no reviews at the moment.')
+
+    def test_view_business_reviews(self):
+        """Test whether view_business_reviews method returns reviews."""
+
+        self.reviews.add_review(1, 'first review', '18-03-17')
+        self.assertEqual(self.reviews.view_business_reviews(1), [{'review': 'first review', 'created_at': '18-03-17'}])
+
+
 if __name__ == '__main__':
     unittest.main()
