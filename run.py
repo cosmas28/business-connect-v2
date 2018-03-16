5dcccc9119fe0ea17254f7d39ad0fc4a2d51f001
cosmas28 <augubilla100@gmail.com>
2018-03-16 22:41:46 +0300

    Register API endpoint to Flask-restful [#155573993]

--- run.py
+++ run.py
@@ -3,10 +3,12 @@ from flask import Flask
 
 from app.resources.business import business_api
 from app.resources.user import user_api
+from app.resources.reviews import reviews_api
 
 app = Flask(__name__)
 app.register_blueprint(business_api, url_prefix='/api/v1')
 app.register_blueprint(user_api, url_prefix='/api/v1')
+app.register_blueprint(reviews_api, url_prefix='/api/v1')
 
 
 @app.route('/')
