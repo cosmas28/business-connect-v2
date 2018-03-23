c4e5805c3a081d14f91e2686027d584a08358755
cosmas28 <augubilla100@gmail.com>
2018-03-23 11:02:08 +0300

    Create the app function [#156200953]

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,38 @@
-from app.models.business import Business
+import os
 
-business = Business()
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+# from app.resources.business import business_api
+# from app.resources.user import user_api
+# from app.resources.reviews import reviews_api
+
+from config import config
+
+db = SQLAlchemy()
+
+
+def create_app():
+    if os.environ.get('FLASK_CONFIG') == 'production':
+        app = Flask(__name__)
+        app.config.update(
+            SECRET_KEY=os.environ.get('SECRET_KEY'),
+            SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI')
+        )
+    else:
+        app = Flask(__name__, instance_relative_config=True)
+        app.config.from_object(config['development'])
+        app.config.from_pyfile('config.py')
+
+    db.init_app(app)
+
+    migrate = Migrate()
+
+    from app import models
+
+    # app.register_blueprint(business_api, url_prefix='/api/v1')
+    # app.register_blueprint(user_api, url_prefix='/api/v1')
+    # app.register_blueprint(reviews_api, url_prefix='/api/v1')
+
+    return app
