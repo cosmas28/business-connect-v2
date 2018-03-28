85dcaada04d6a28a011939fba9e80c7e900480dc
cosmas28 <augubilla100@gmail.com>
2018-03-28 11:57:42 +0300

    Modify test case for view all businesses API endpoint to use postgresql [#156200953]

--- tests/test_business_api.py
+++ tests/test_business_api.py
@@ -37,8 +37,8 @@ class AbstractTest(unittest.TestCase):
         business_data = json.dumps({'name': 'CosmasTech', 'category': 'Technology', 'location': 'Nairobi',
                                     'summary': 'AI is transforming human life'})
 
-        self.run_app.post('/api/v1/businesses', data=business_data,
-                          headers=dict(Authorization="Bearer " + access_token))
+        return self.run_app.post('/api/v1/businesses', data=business_data,
+                                 headers=dict(Authorization="Bearer " + access_token))
 
     def tearDown(self):
         """Call after every test to remove the created table."""
@@ -145,6 +145,22 @@ class CreateBusinessTest(AbstractTest):
 
         self.assertEqual(json.loads(response.data.decode())['response_message'],
                          'Business has been registered successfully!')
+
+    def test_user_can_view_all_businesses(self):
+        """Test whether a user can view all registered businesses."""
+
+        self.register_user()
+        login_response = self.login_user()
+        access_token = json.loads(login_response.data.decode())['access_token']
+
+        business_data = json.dumps({'name': 'Palmer Tech', 'category': 'Technology', 'location': 'Mombasa',
+                                    'summary': 'IoT is transforming human security'})
+        self.run_app.post('/api/v1/businesses', data=business_data,
+                          headers=dict(Authorization="Bearer " + access_token))
+
+        response = self.run_app.get('/api/v1/businesses', headers=dict(Authorization="Bearer " + access_token))
+
+        self.assertIn('Palmer Tech', str(response.data))
 
 
 if __name__ == '__main__':
