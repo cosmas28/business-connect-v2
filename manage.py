32783800eab1a1b495f76d6a044c760307e23b8c
cosmas28 <augubilla100@gmail.com>
2018-04-09 17:14:12 +0300

    Create a python REPL

--- manage.py
+++ manage.py
@@ -1,13 +1,22 @@
 from flask_script import Manager
 from flask_migrate import MigrateCommand
 
+from flasgger import Swagger
+
 from app import create_app
 
 
 app = create_app('config.DevelopmentConfig')
 manager = Manager(app)
+swagger = Swagger(app)
 
 manager.add_command('db', MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    """Creates a python REPL"""
+
+    return dict(app=app)
 
 if __name__ == "__main__":
     manager.run()
