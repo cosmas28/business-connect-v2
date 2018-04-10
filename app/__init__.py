129bf7670dd3db2dc64192d0bc5ad372022ba673
cosmas28 <augubilla100@gmail.com>
2018-04-10 12:37:21 +0300

    Fix travis error

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,6 @@ def create_app(config_object):
 
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_object])
-    app.config.from_pyfile("config.py")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
