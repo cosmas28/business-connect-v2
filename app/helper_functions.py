2f49e4530806755070fbf5a6d75cc17e16623bcd
cosmas28 <augubilla100@gmail.com>
2018-04-13 10:42:07 +0300

    Define resusable helper methods

---
+++ app/helper_functions.py
@@ -0,0 +1,28 @@
+"""Create helper functions to be used in views module."""
+
+from app.models import User
+
+
+def email_exist(email):
+    """Check if user email is already registered."""
+
+    user_email = User.query.filter_by(email=email).first()
+    return user_email
+
+
+def username_exist(user_name):
+    """Check if username is already registered."""
+
+    user_name = User.query.filter_by(username=user_name.lower()).first()
+    return user_name
+
+
+def valid_password(password, confirm_password):
+    """Check whether the password have more than 6 characters."""
+
+    if len(password) <= 6:
+        response_message = {'message': 'Password must be more than 6 characters!'}
+        return response_message
+    if password not in confirm_password:
+        response_message = {'message': 'Password does not match the confirmation password!'}
+        return response_message
